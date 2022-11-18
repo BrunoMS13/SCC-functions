5c75c005c9d2aa55040b87fa4ea1b2669a28fef1
BrunoMS13 <91726785+BrunoMS13@users.noreply.github.com>
2022-11-18 19:21:52 +0000

    Set timer once every five minutes, to no get spam.

--- src/main/java/scc/serverless/TimerFunction.java
+++ src/main/java/scc/serverless/TimerFunction.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
 public class TimerFunction {
     @FunctionName("auction-closer")
     public void cosmosFunction( @TimerTrigger(name = "periodicSetTime",
-    								schedule = "*/1 * * * * *")
+    								schedule = "0 */5 * * * *")
     				String timerInfo,
     				ExecutionContext context) {
 
@@ -31,25 +31,24 @@ public class TimerFunction {
 		Logger.getGlobal().info("------ Running Through Auctions ------");
 		while (it.hasNext()) {
 			AuctionDAO a = it.next();
-			Logger.getGlobal().info(a.getStatus());
-			Logger.getGlobal().info(a.getEndingTime().getTime() + "<" + System.currentTimeMillis());
 			if (a.getEndingTime().getTime() < System.currentTimeMillis() && !a.getStatus().equals("CLOSED")) {
 				Logger.getGlobal().info("Auction about to be closed: " + a);
 				a.setStatus("CLOSED");
 				Collection<Bid> bids = a.getBids().values();
 				float winnerBid = 0;
 				Logger.getGlobal().info("------ Running Through Auction Bids ------");
-				for (Bid bid: bids) {
-					Logger.getGlobal().info(bid.toString());
-					float curBid = bid.getBidValue();
-					if (curBid > winnerBid) {
-						winnerBid = bid.getBidValue();
-						a.setWinnerId(bid.getUserId());
+				try {
+					for (Bid bid : bids) {
+						float curBid = bid.getBidValue();
+						if (curBid > winnerBid) {
+							winnerBid = bid.getBidValue();
+							a.setWinnerId(bid.getUserId());
+						}
 					}
-				}
+					Logger.getGlobal().info("Winner= " + a.getWinnerId());
+				} catch (Exception e) {}
 				db.updateAuction(a);
 				rl.updateAuction(a);
-				Logger.getGlobal().info("Winner= " + a.getWinnerId());
 			}
 			Logger.getGlobal().info("\n");
 		}
